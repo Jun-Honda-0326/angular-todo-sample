a0323a173995f1bed27868b37a3303b8d8d4516c
Jun-Honda-0326 <60055417+Jun-Honda-0326@users.noreply.github.com>
2020-12-05 22:33:57 +0900

    add task form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,4 +11,11 @@ export class AppComponent {
     {title: '可燃ごみを出す', done: true},
     {title: '銀行に行く', done: false},
   ];
+
+  newTaskTitle = '';
+
+  addTask(){
+    this.tasks.push({title: this.newTaskTitle, done: false})
+    this.newTaskTitle = '';
+  }
 }
